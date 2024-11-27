088cc3b8af4d80c7ac2f6dce72d683359bbccd1d
rodanAsh <aakashash2002@gmail.com>
2024-11-27 12:21:30 +0530

    feat ~ designed home page

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login'
 import Navbar from './components/Navbar'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
@@ -20,8 +21,9 @@ const App = () => {
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/login' element={<Login />} />
-        <Route path='my-profile' element={<MyProfile />} />
-        <Route path='my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
     </main>
   )
