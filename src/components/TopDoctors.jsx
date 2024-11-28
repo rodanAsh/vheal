f1704aa6b7c29c1ec245036eb3878dcd77e88251
rodanAsh <aakashash2002@gmail.com>
2024-11-28 20:00:05 +0530

    designed banner section in home page

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
-import { doctors } from '../assets/assets'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const TopDoctors = () => {
+  const {doctors} = useContext(AppContext)
   const navigate = useNavigate()
   return (
     <section className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
@@ -11,7 +12,7 @@ const TopDoctors = () => {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {
           doctors.slice(0,10).map((doctor,index) => (
-            <div onClick={() => navigate(`/appointment/${doctor._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500' key={doctor._id}>
+            <div key={doctor._id} onClick={() => navigate(`/appointment/${doctor._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
               <img className='bg-blue-50' src={doctor.image} alt="" />
               <div className='p-4'>
                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
@@ -24,7 +25,7 @@ const TopDoctors = () => {
           ))
         }
       </div>
-      <button className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+      <button onClick={() => {navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </section>
   )
 }
