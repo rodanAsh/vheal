b22eea15ceabead8d63f8c181a87619fdb04c006
rodanAsh <aakashash2002@gmail.com>
2024-12-04 16:25:24 +0530

    feat ~ designed doctors page

--- src/pages/Doctors.jsx
+++ src/pages/Doctors.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Doctors = () => {
+  const { speciality } = useParams()
+  const [filteredDoc,setFilteredDoc] = useState([])
+  const {doctors} = useContext(AppContext)
+  const navigate = useNavigate()
+
+  const applyFilter = () => {
+    if(speciality){
+      setFilteredDoc(doctors.filter(doc => doc.speciality === speciality))
+    } else{
+      setFilteredDoc(doctors)
+    }
+  }
+
+  useEffect(() => {
+    applyFilter()
+  },[doctors,speciality])
+  
   return (
-    <div>Doctors</div>
+    <section>
+      <p className='text-gray-600'>Browse through the doctors specialist.</p>
+      <div className='flex flex-col sm:flex-row items-start gap-5 mt-5'>
+        {/* ---------left side ---------- */}
+        <div className='flex flex-col gap-4 text-sm text-gray-600'>
+          <p onClick={() => speciality === 'General physician' ? navigate('/doctors') : navigate('/doctors/General physician')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'General physician' ? 'bg-indigo-100 text-black' : ""}`}>General physician</p>
+          <p onClick={() => speciality === 'Gynecologist' ? navigate('/doctors') : navigate('/doctors/Gynecologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gynecologist' ? 'bg-indigo-100 text-black' : ""}`}>Gynecologist</p>
+          <p onClick={() => speciality === 'Dermatologist' ? navigate('/doctors') : navigate('/doctors/Dermatologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Dermatologist' ? 'bg-indigo-100 text-black' : ""}`}>Dermatologist</p>
+          <p onClick={() => speciality === 'Pediatricians' ? navigate('/doctors') : navigate('/doctors/Pediatricians')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Pediatricians' ? 'bg-indigo-100 text-black' : ""}`}>Pediatricians</p>
+          <p onClick={() => speciality === 'Neurologist' ? navigate('/doctors') : navigate('/doctors/Neurologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Neurologist' ? 'bg-indigo-100 text-black' : ""}`}>Neurologist</p>
+          <p onClick={() => speciality === 'Gastroenterologist' ? navigate('/doctors') : navigate('/doctors/Gastroenterologist')} className={`w-[94vw] sm:w-auto pl-3 py-1.5 pr-16 border border-gray-300 rounded transition-all cursor-pointer ${speciality === 'Gastroenterologist' ? 'bg-indigo-100 text-black' : ""}`}>Gastroenterologist</p>
+        </div>
+        {/* ------- right side -------- */}
+        <div className='w-full grid grid-cols-auto gap-4 gap-y-6'>
+          {
+            filteredDoc.map((doc,index) => (
+              <div key={doc._id} onClick={() => navigate(`/appointment/${doc._id}`)} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500'>
+              <img className='bg-blue-50' src={doc.image} alt="" />
+              <div className='p-4'>
+                <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                  <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                </div>
+                <p className='text-gray-900 text-lg font-medium'>{doc.name}</p>
+                <p className='text-gray-600 text-sm'>{doc.speciality}</p>
+              </div>
+            </div>
+            ))
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
