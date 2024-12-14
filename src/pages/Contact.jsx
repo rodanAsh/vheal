ee16d2c823955c586b806a71a412136ff9291e4a
rodanAsh <aakashash2002@gmail.com>
2024-12-14 19:00:44 +0530

    feat - UI desihn completed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
             Tel: (000) 000-0000 <br />
             Email: aakashashdev@gmail.com
           </p>
-          <b className='font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</b>
+          <b className='font-semibold text-lg text-gray-600'>CAREERS AT VHEAL</b>
           <p className='text-gray-500'>Learn more about our teams and job openings.</p>
           <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
         </div>
